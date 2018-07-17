3c21fb88a87d3ef3df3e5607e3c9ee4cab21260d
teri.mathews <teri.mathews@caliberhomeloans.com>
2018-07-16 18:50:20 -0600

    added mdbootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import {BrowserModule} from '@angular/platform-browser';
-import {NoopAnimationsModule} from "@angular/platform-browser/animations/src/module";
-import {NgModule} from '@angular/core';
+// import {NoopAnimationsModule} from "@angular/platform-browser/animations/src/module";
+import {NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {HttpClientModule} from "@angular/common/http";
-import {HttpModule} from '@angular/http';
 import {ReactiveFormsModule} from "@angular/forms";
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import {InMemoryWebApiModule} from "angular-in-memory-web-api";
 import {PortfolioData} from "./shared/models/portfolio-data";
 
@@ -12,7 +13,7 @@ import {AppMaterialModules} from './material.module';
 import {AppComponent} from './app.component';
 import {AboutMeComponent} from './about-me/about-me.component';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
-import {AppRoutingModule} from './/app-routing.module';
+import {AppRoutingModule} from './app-routing.module';
 import {HomeComponent} from './home/home.component';
 import {SkillsComponent} from './skills/skills.component';
 import {WorkComponent} from './work/work.component';
@@ -20,8 +21,6 @@ import {ContactComponent} from './contact/contact.component';
 import {DateTimeNowService} from './shared/date-time-now.service';
 import {PersonalProjectsComponent} from './personal-projects/personal-projects.component';
 import {ProfessionalProjectsComponent} from './professional-projects/professional-projects.component';
-import {PortfolioService} from "./shared/services/portfolio.service";
-import {PersonalProjectsEditComponent} from './personal-projects/personal-projects-edit/personal-projects-edit.component';
 
 
 @NgModule({
@@ -35,14 +34,14 @@ import {PersonalProjectsEditComponent} from './personal-projects/personal-projec
     ContactComponent,
     PersonalProjectsComponent,
     ProfessionalProjectsComponent,
-    PersonalProjectsEditComponent
   ],
   imports: [
-    BrowserModule, BrowserAnimationsModule, AppRoutingModule, ReactiveFormsModule,
+    BrowserModule, BrowserAnimationsModule, AppRoutingModule, ReactiveFormsModule, MDBBootstrapModule.forRoot(), NgbModule.forRoot(),
     InMemoryWebApiModule.forRoot(PortfolioData),
-    AppMaterialModules, AppRoutingModule, HttpModule, HttpClientModule
+    AppMaterialModules, AppRoutingModule, HttpClientModule
   ],
-  providers: [DateTimeNowService, PortfolioService],
+  schemas: [ NO_ERRORS_SCHEMA ],
+  providers: [DateTimeNowService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
